1ded119d8a604c82a686c2132679e805f1cc761d
vhyz <32564682@qq.com>
2019-06-23 22:11:30 +0800

    modify structure

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <time.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "requests.h"
+
+int main() {
+  RequestOption requestOption;
+  requestOption.headers["User-Agent"] =
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+      "Chrome/74.0.3729.157 Safari/537.36";
+  HttpResponsePtr r = get("https://www.baidu.com", requestOption);
+  std::cout << r->text << std::endl;
+  std::cout << r->statusCode << std::endl;
+  return 0;
+}
