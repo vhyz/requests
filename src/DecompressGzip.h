e76b78d7d6f24b3d7a1cbff9beff0fc2dd432dde
vhyz <32564682@qq.com>
2019-06-30 02:16:03 +0800

    fix

--- src/DecompressGzip.h
+++ src/DecompressGzip.h
@@ -4,8 +4,13 @@
 // decompress gzip data using zlib
 //
 
+#ifndef REQUESTS_DECOMPRESSGZIP_H
+#define REQUESTS_DECOMPRESSGZIP_H
+
 #include <stdio.h>
 #include <string>
 
 bool decompressGzip(const std::string& compressedString,
                     std::string& decompressedString);
+
+#endif
